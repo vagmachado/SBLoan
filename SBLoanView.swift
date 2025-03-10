8d1e1400c0908a7e98bb0b9d698d77a6f841b848
Vagner Machado <82machado@gmail.com>
2025-03-10 18:34:23 -0300

    first commit

---
+++ SBLoanView.swift
@@ -0,0 +1,18 @@
+//
+//  SBLoanView.swift
+//  Pods
+//
+//  Created by Vagner Machado on 10/03/25.
+//
+
+import SwiftUI
+
+struct SBLoanView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SBLoanView()
+}
